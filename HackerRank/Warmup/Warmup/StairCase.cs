35d4d8b6216b825fd517763404f724506926e8fc
Sravs <Sravs@DESKTOP-E7DS46C>
2017-03-25 17:55:00 -0400

    Check

---
+++ HackerRank/Warmup/Warmup/StairCase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warmup
+{
+    class StairCase
+    {
+        static void Main(String[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = n; j >= 1; j--)
+                {
+                    if (i >= j)
+                        Console.Write("#");
+                    else
+                        Console.Write(" ");
+                }
+                Console.Write("\n");
+            }
+        }
+     }
+
+}
